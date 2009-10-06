bbc9a45c9600dc6e4468f2792308d037c11afb3e
Jason Zhang <zhihengz@gmail.com>
2009-10-06 00:14:18 -0500

    move ksparser code into ksxml2cfg

--- src/ksxml2cfg.py
+++ src/ksxml2cfg.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python -t
 import sys,os
+from rhks import parser,components
+
 def print_usage():
 	print "usage: ksxml2cfg <xml file>"
+
+def build( kickstart, filename ):
+    file = open(filename, "w" )
+    for command in kickstart.commands:
+        file.writelines( command.compile() )
+    file.close
 
 def main():
 	if len(sys.argv) < 2:
 		print_usage()
 		sys.exit(1)
-	print "done"
+	ks = parser.parseKickstartXmlSource( sys.argv[1] )
+	build(ks, ks.name + ".cfg" )
 
 if __name__ == "__main__":
 	main()
