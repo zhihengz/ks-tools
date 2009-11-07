80564b9aa0b6caba88b3bc69912ef39816e21f29
Jason Zhang <zhihengz@gmail.com>
2009-11-06 18:06:30 -0600

    added init comps parse code

---
+++ src/rhks/comps.py
@@ -0,0 +1,31 @@
+import xml.dom.minidom
+import log
+
+def getNodeText( aNode):
+    """get node text
+    
+    Arguments:
+    - `aNode`:
+    """
+    if aNode.nodeType == aNode.TEXT_NODE:
+        return aNode.nodeValue
+    if aNode.nodeType == aNode.ELEMENT_NODE:
+        for childNode in aNode.childNodes:
+            childNodeText = getNodeText( childNode )
+            if not childNodeText == None:
+                return childNodeText
+
+    return None
+def parsePackages( node ):
+    pkgset=[]
+    if node.localName == 'comps':
+        for pkgreqNode in node.getElementsByTagName( 'packagereq' ):
+          pkg = parsePackageReq( pkgreqNode )
+          if not pkg in pkgset:
+              pkgset.append( pkg )
+
+    return pkgset
+
+def parsePackageReq( node ):
+    return getNodeText( node )
+        
